775e8228f048250b4ff32949e2af49c57634c28c
hhjuliet <280157545@qq.com>
2017-01-05 17:35:50 +0800

    insertKVM

--- test/src/test/java/org/zstack/test/core/TestEncrypt.java
+++ test/src/test/java/org/zstack/test/core/TestEncrypt.java
@@ -3,6 +3,7 @@ package org.zstack.test.core;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.zstack.core.cloudbus.CloudBus;
 import org.zstack.core.componentloader.ComponentLoader;
 import org.zstack.core.db.DatabaseFacade;
 import org.zstack.core.encrypt.EncryptRSA;
@@ -10,6 +11,7 @@ import org.zstack.header.core.encrypt.DECRYPT;
 import org.zstack.header.core.encrypt.ENCRYPT;
 import org.zstack.kvm.KVMHostVO;
 import org.zstack.test.BeanConstructor;
+import org.zstack.test.deployer.Deployer;
 
 /**
  * Created by mingjian.deng on 16/11/2.
@@ -19,12 +21,24 @@ public class TestEncrypt {
     ComponentLoader loader;
     EncryptRSA rsa;
     DatabaseFacade dbf;
+    Deployer deployer;
+
 
     @Before
     public void setUp() throws Exception {
         BeanConstructor con = new BeanConstructor();
         loader = con.build();
         rsa = loader.getComponent(EncryptRSA.class);
+
+        /*deployer = new Deployer("deployerXml/mevoco/TestMevoco.xml", con);
+        deployer.addSpringConfig("mevocoRelated.xml");
+        deployer.addSpringConfig("billing.xml");
+        deployer.load();*/
+
+        loader = deployer.getComponentLoader();
+        //bus = loader.getComponent(CloudBus.class);
+        dbf = loader.getComponent(DatabaseFacade.class);
+
     }
 
     public String getPassword() {
@@ -56,6 +70,7 @@ public class TestEncrypt {
 
         setPassword("test_update");
         Assert.assertEquals("test_update", getString());
+
         KVMHostVO kvmHostVO = new KVMHostVO();
         kvmHostVO.setUsername("test");
         kvmHostVO.setPassword("password");
