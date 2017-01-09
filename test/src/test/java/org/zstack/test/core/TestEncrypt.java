fe065710e1f34c26837698aec04d47da180f4abc
hhjuliet <280157545@qq.com>
2017-01-09 11:24:10 +0800

    sql test

--- test/src/test/java/org/zstack/test/core/TestEncrypt.java
+++ test/src/test/java/org/zstack/test/core/TestEncrypt.java
@@ -143,17 +143,19 @@ public class TestEncrypt {
         config.totalMemory = SizeUnit.GIGABYTE.toByte(8);
         config.usedMemory = SizeUnit.MEGABYTE.toByte(512);
 
-        String uuid = addHost().getUuid();
+        String result = updateKey();
+
+        /*String uuid = addHost().getUuid();
         HostCapacityVO hvo = dbf.findByUuid(uuid, HostCapacityVO.class);
 
-        KVMHostVO kvmHostVO = new KVMHostVO();
+        KVMHostVO kvmHostVO = new KVMHostVO();*/
         /*HostInventory k = deployer.hosts.get("host1");
         String uuid = k.getUuid();*/
 
-        logger.debug("uuid is: "+uuid);
-        kvmHostVO.setUuid(uuid);
+        logger.debug("result is: "+result);
+        /*kvmHostVO.setUuid(uuid);
         kvmHostVO.setUsername("test");
-        kvmHostVO.setPassword("password");
+        kvmHostVO.setPassword("password");*/
         //dbf.persist(kvmHostVO);
     }
 }
